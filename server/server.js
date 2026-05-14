1727bafba251ff5c434d10461533f651e96851b6
priyankasb5055 <priyankasb5055@gmail.com>
2026-05-14 09:04:44 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -17,10 +17,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5175'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://mindcare-ai-client.onrender.com",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
